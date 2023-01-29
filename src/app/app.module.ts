4edfa34e38788e0cffb8db6825f40a8c8c3a670c
Skerz <claudequarante@gmail.com>
2023-01-29 12:35:48 +0100

    s2.23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './serveur/server.component';
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
